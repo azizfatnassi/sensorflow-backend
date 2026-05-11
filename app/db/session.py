b099a9e34eb159e4a92e76b9849db04f02f9d286
azizfatnassi <mohamedaziz.fatnassi@esprit.tn>
2026-05-12 00:00:03 +0100

    fix: add pool_pre_ping and pool_recycle for Neon

--- app/db/session.py
+++ app/db/session.py
@@ -3,15 +3,17 @@ from sqlalchemy.orm import sessionmaker
 
 from app.core.config import settings
 
-engine = create_engine(settings.DATABASE_URL,
-                       connect_args={"sslmode": "require",
-                                      pool_pre_ping=True,
-                                      pool_recycle=300, })
+engine = create_engine(
+    settings.DATABASE_URL,
+    connect_args={"sslmode": "require"},
+    pool_pre_ping=True,
+    pool_recycle=300,
+)
 
-SessionLocal = sessionmaker ( autocommit = False, autoflush= False , bind= engine )
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db():
-    db= SessionLocal()
+    db = SessionLocal()
     try:
         yield db
     finally:
